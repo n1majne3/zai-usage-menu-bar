30f981a70d75f154cbbabc80413bab46c19a10f5
benjamin <benjamin@localhost>
2026-04-04 22:53:29 +0800

    Apply language preference on app launch

--- ZaiUsageMenuBar/Sources/ZaiUsageMenuBar/ZaiUsageMenuBarApp.swift
+++ ZaiUsageMenuBar/Sources/ZaiUsageMenuBar/ZaiUsageMenuBarApp.swift
@@ -3,10 +3,23 @@ import SwiftUI
 @main
 struct ZaiUsageMenuBarApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @AppStorage("preferredLanguage") private var preferredLanguage: String = "system"
+    
+    init() {
+        applyLanguagePreference()
+    }
     
     var body: some Scene {
         Settings {
             SettingsView()
         }
+    }
+    
+    private func applyLanguagePreference() {
+        guard preferredLanguage != "system" else { return }
+        
+        let languageCode = preferredLanguage == "zh-Hans" ? "zh-Hans" : "en"
+        UserDefaults.standard.set([languageCode], forKey: "AppleLanguages")
+        UserDefaults.standard.synchronize()
     }
 }
